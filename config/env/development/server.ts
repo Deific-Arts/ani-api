0f3f548a93a78be65071bc9179b0e50931261f64
Hasani Rogers <hasani.rogers@gmail.com>
2025-01-13 10:23:54 -0500

    ??

--- config/env/development/server.ts
+++
@@ -1,3 +0,0 @@
-export default ({ env }) => ({
-  port: '1338',
-});
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,14 @@ module.exports = {
             name: 'anibookquotes',
             script: 'npm',
             args: 'start',
-        },
+            env_production: {
+              NODE_ENV: 'production',
+              PORT: 1337,
+            },
+            env_development: {
+              NODE_ENV: 'development',
+              PORT: 1338
+            }
+        }
     ],
 };
